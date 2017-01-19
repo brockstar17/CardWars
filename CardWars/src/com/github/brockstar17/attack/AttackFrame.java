9c7a80cc2f08f456d364d9ce14c52e7a300e44f5
brockstar17 <brockdyer.git@gmail.com>
2017-01-19 07:31:40 -0500

    Commented out debug lines

--- CardWars/src/com/github/brockstar17/attack/AttackFrame.java
+++ CardWars/src/com/github/brockstar17/attack/AttackFrame.java
@@ -11,36 +11,37 @@ import com.github.brockstar17.PlayingCard;
 import com.github.brockstar17.util.ImageUtils;
 
 @SuppressWarnings("serial")
-public class AttackFrame extends JFrame implements WindowListener{
-	
+public class AttackFrame extends JFrame implements WindowListener
+{
+
 	private CardWars cw;
 	private AttackPanel ap;
 	private int w;
 	private int l;
-	
+
 	protected String winner;
-	
+
 	private PlayingCard player, opp;
-	
+
 	public AttackFrame(CardWars cw, PlayingCard player, PlayingCard other)
 	{
 		super("Battle Commences");
-		
+
 		this.cw = cw;
 		this.player = player;
 		this.opp = other;
-		
-		this.w = (int)(cw.getWidth()*.75);
+
+		this.w = (int) (cw.getWidth() * .75);
 		this.l = ImageUtils.calcWidth(400, this.w, 200);
 
 		addWindowListener(this);
-		
+
 		setSize(w, l);
-		
+
 		this.ap = new AttackPanel(this);
 		Container c = getContentPane();
 		c.add(ap);
-		
+
 		setLocationRelativeTo(null);
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -50,73 +51,68 @@ public class AttackFrame extends JFrame implements WindowListener{
 
 	@Override
 	public void windowActivated(WindowEvent e) {
-		
-		
+
 	}
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		
+
 		if(winner == "p2")
 		{
-			System.out.println("Lost: P1");
+			// System.out.println("Lost: P1");
 			CardWars.playerDiscard.add(player);
 		}
 		else if(winner == "p1")
 		{
-			System.out.println("Lost: P2");
+			// System.out.println("Lost: P2");
 			CardWars.otherDiscard.add(opp);
 		}
 		this.cw.setVisible(true);
 		this.cw.setEnabled(true);
-		
+
 	}
 
 	@Override
 	public void windowClosing(WindowEvent e) {
-		
-		
+
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
-		
-		
+
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
-		
-		
+
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
-		
-		
+
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {
-		
-		
+
 	}
-	
-	public PlayingCard getPlayerCard(){
-		
-		return this.player;	
+
+	public PlayingCard getPlayerCard() {
+
+		return this.player;
 	}
-	
-	public PlayingCard getOppCard(){
+
+	public PlayingCard getOppCard() {
 		return this.opp;
 	}
-	
-	public int getW(){
-		//System.out.println("w " + this.w);
+
+	public int getW() {
+		// System.out.println("w " + this.w);
 		return this.w;
 	}
-	public int getL(){
-		//System.out.println("l " + this.l);
+
+	public int getL() {
+		// System.out.println("l " + this.l);
 		return this.l;
 	}
 
